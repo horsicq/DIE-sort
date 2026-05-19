77c135c9433a707d9d2b73a6259ba82a5172113c
horsicq <horsicq@gmail.com>
2026-05-19 18:54:05 +0200

    feat: add license, readme, and translation files

---
+++ translations/die-sort_pt_BR.ts
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>DialogScanProgress</name>
+    <message>
+        <location filename="../src/gui/dialogscanprogress.ui" line="14"/>
+        <source>Scan progress</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/dialogscanprogress.ui" line="60"/>
+        <source>Total</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/dialogscanprogress.ui" line="106"/>
+        <source>Current</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/dialogscanprogress.ui" line="152"/>
+        <source>Threads</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/dialogscanprogress.ui" line="245"/>
+        <source>%v/%m</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/dialogscanprogress.ui" line="267"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GuiMainWindow</name>
+    <message>
+        <location filename="../src/gui/guimainwindow.ui" line="33"/>
+        <location filename="../src/gui/guimainwindow.cpp" line="51"/>
+        <source>Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/guimainwindow.ui" line="53"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/guimainwindow.cpp" line="51"/>
+        <source>Bugreports: horsicq@gmail.com</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
